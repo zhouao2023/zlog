8f17491490cf13892a3ad22f999eb0937fe22123
speed <zhousongyu@qq.com>
2023-12-26 14:33:01 +0800

    测试发布

--- src/Test.php
+++ src/Test.php
@@ -7,10 +7,21 @@ class Test
 
 {
 
-    public function index()
-
+    //添加一个日志用于调试
+    protected function zlog($data,$type='pay')
     {
+        $data = is_array($data) ? print_r($data, true) : $data;
 
-        echo 'composer包测试成功';
+        $filename = dirname(__DIR__).DS.'runtime/zlog/'.date("Y-m-d").$type.'.txt';
+        // 判断文件夹是否存在
+        if (!file_exists($filename)) {
+            // 创建文件
+            $file = fopen($filename, 'w');
+            fclose($file);
+
+            // 设置文件权限为 777
+            chmod($filename, 0777);
+        }
+        file_put_contents($filename, date("Y-m-d H:i:s") . " " . $data . "\r\n", FILE_APPEND | LOCK_EX);
     }
 }
